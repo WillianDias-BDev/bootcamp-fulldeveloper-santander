abc4473b7c491300d565b1abfcb26846f0807ec1
WillianDias <williandavid1092@gmail.com>
2023-08-28 12:26:21 -0300

    adicionado-aulas-javascript

---
+++ sintaxe-basica-em-javascript/aula-02-entendendo-variaveis-e-seus-valores/script.js
@@ -0,0 +1,20 @@
+//tipos primitivos
+//o metodo typeof exibe o tipo da variavel
+
+//um boolean retorna veridadeiro ou falso
+var verdadeiroOuFalso = false;
+console.log(typeof (verdadeiroOuFalso));
+
+//tipo number
+var nummeroQualquer = 1;
+console.log(typeof (nummeroQualquer));
+
+//tipo string
+var nome = "Willian";
+console.log(typeof (nome));
+
+//var = escopo global e local, pode ter seu valor alterado, 
+//se não tiver um valor inicial será tratada como null;
+var variavel = "Willian"
+variavel = "David"
+console.log(variavel)
