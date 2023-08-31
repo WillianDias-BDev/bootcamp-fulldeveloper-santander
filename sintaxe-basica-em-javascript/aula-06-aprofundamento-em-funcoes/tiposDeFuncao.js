fae7d702f76f36a40aeab1210259bd23de92e860
WillianDias <williandavid1092@gmail.com>
2023-08-31 13:11:50 -0300

    aula-06-adicionada

---
+++ sintaxe-basica-em-javascript/aula-06-aprofundamento-em-funcoes/tiposDeFuncao.js
@@ -0,0 +1,24 @@
+// //tipos de funcao
+
+// //funcao declarativas
+
+// function funcao() {
+//     console.log('tudo certo jovem')
+// }
+
+// funcao();
+
+// //expressao funcao
+// //com nomeação
+// var funcao = function () {
+//     console.log('sou uma funcao de expressão.')
+// }
+
+// funcao()
+
+// //arrow function
+// var funcao = () => {
+//     console.log('sou uma arrow function');
+// }
+
+// funcao()
