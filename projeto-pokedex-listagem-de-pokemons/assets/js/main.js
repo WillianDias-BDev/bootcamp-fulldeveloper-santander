f4eee8f3c265e189568a2624c77a4509a1dcee80
WillianDias <williandavid1092@gmail.com>
2023-09-08 15:30:22 -0300

    projeto-atualizado

--- projeto-pokedex-listagem-de-pokemons/assets/js/main.js
+++ projeto-pokedex-listagem-de-pokemons/assets/js/main.js
@@ -1,15 +1,17 @@
+function convertPokemonToTypeLi(pokemonType) {
+    return pokemonType.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLi(pokemon) {
     return `
             <li class="pokemon">
-                <span class="number">#001</span>
+                <span class="number">#${pokemon.order.toString().padStart(3, '0')}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
-
-                    </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                        ${convertPokemonToTypeLi(pokemon.types).join('')}
+                     </ol>
+                    <img src="${pokemon.sprites.other.dream_world.front_default}"
                         alt="${pokemon.name}">
 
 
--- projeto-pokedex-listagem-de-pokemons/assets/js/poke-api.js
+++ projeto-pokedex-listagem-de-pokemons/assets/js/poke-api.js
@@ -1,10 +1,30 @@
 
 const pokeApi = {}
 
+pokeApi.getPokemonsDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `http://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.error(error))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonDetails) => pokemonDetails)
+
+
+
 }
+Promise.all([
+    fetch('http://pokeapi.co/api/v2/pokemon/1'),
+    fetch('http://pokeapi.co/api/v2/pokemon/2'),
+    fetch('http://pokeapi.co/api/v2/pokemon/3'),
+    fetch('http://pokeapi.co/api/v2/pokemon/4'),
+    fetch('http://pokeapi.co/api/v2/pokemon/5'),
+]).then((results) => {
+    console.log(results)
+})
