7c2005eabc2d223d5fd0f793835b9d739ba2d28a
WillianDias <williandavid1092@gmail.com>
2023-10-07 19:56:06 -0300

    projeto-atualizado

--- projeto-pokedex-minimalista/assets/js/pokemon-detail.js
+++ projeto-pokedex-minimalista/assets/js/pokemon-detail.js
@@ -6,12 +6,36 @@ document.addEventListener("DOMContentLoaded", () => {
     const id = parseInt(pokemonID, 10);
 
     if (id < 1 || id > MAX_POKEMONS) {
-        return (window.location.href = "./index.html");
+        return (window.location.href = "../index.html");
     }
 
     currentPokemonId = id;
     loadPokemon(id);
 });
+
+async function loadPokemon(id) {
+    try {
+        const [pokemon, pokemonSpecies] = await Promise.all([
+            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! Status: ${res.status}`);
+                }
+                return res.json();
+            }),
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! Status: ${res.status}`);
+                }
+                return res.json();
+            }),
+        ]);
+
+        // Resto do seu código aqui...
+    } catch (error) {
+        console.error("An error occured while fetching Pokemon data:", error);
+        return false;
+    }
+}
 
 async function loadPokemon(id) {
     try {
